6d3fd55935c8126a05ce68cf17899d9282550e1c
Matiaspotti <matias.pugliese@hotmail.com>
2020-01-02 21:05:28 -0300

    Probando test

---
+++ 00-ProbandoTests/test.js
@@ -0,0 +1,12 @@
+function saludar(nombre) {
+  if (nombre===undefined) {
+    return 'Hola!'
+  }
+  else {
+    return 'Hola, '+nombre+"!"
+  }
+}
+
+function sumar(num, numero) {
+  return num+numero
+}
